59d345a0ddd195ee26461e38d908e55dd1ffc480
Leandro Alves Dourado Arcanjo <leandrotux@hotmail.com>
2021-03-11 05:45:41 -0300

    Fix questions

--- lib/main.dart
+++ lib/main.dart
@@ -5,44 +5,61 @@ import 'package:projeto_perguntas/Answer.dart';
 main() => runApp(QuestionApp());
 
 class _QuestionAppState extends State<QuestionApp> {
-  var selectedQuestion = 0;
+  var _selectedQuestion = 0;
+
+  final _questions = const [
+    {
+      "text": "Qual é sua cor favorita?",
+      "answers": ["Preto", "Vermelho", "Verde", "Branco"],
+    },
+    {
+      "text": "Qual o seu aninal favorito?",
+      "answers": ["Coelho", "Cobra", "Elefante", "Leão"],
+    },
+    {
+      "text": "Qual o seu instrutor favorito?",
+      "answers": ["Maria", "João", "Leo", "Pedro"],
+    }
+  ];
 
   void _replay() {
-    setState(() {
-      selectedQuestion++;
-    });
+    if (hasSelectedQuestion) {
+      setState(() {
+        _selectedQuestion++;
+      });
+    }
+  }
+
+  bool get hasSelectedQuestion {
+    return _selectedQuestion < _questions.length;
   }
 
   @override
   Widget build(BuildContext context) {
-    final questions = [
-      {
-        "text": "Qual é sua cor favorita?",
-        "answers": ["Preto", "Vermelho", "Verde", "Branco"],
-      },
-      {
-        "text": "Qual o seu aninal favorito?",
-        "answers": ["Coelho", "Cobra", "Elefante", "Leão"],
-      },
-      {
-        "text": "Qual o seu instrutor favorito?",
-        "answers": ["Maria", "João", "Leo", "Pedro"],
-      }
-    ];
-
-    List<String> answers = questions[selectedQuestion]["answers"];
+    List<String> answers =
+        hasSelectedQuestion ? _questions[_selectedQuestion]["answers"] : null;
 
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("Perguntas"),
         ),
-        body: Column(
-          children: <Widget>[
-            Question(questions[selectedQuestion]["text"]),
-            ...answers.map((t) => Answer(t, _replay)).toList(),
-          ],
-        ),
+        body: hasSelectedQuestion
+            ? Column(
+                children: <Widget>[
+                  Question(_questions[_selectedQuestion]["text"]),
+                  ...answers.map((t) => Answer(t, _replay)).toList(),
+                ],
+              )
+            : Center(
+                child: Text(
+                  "Parabéns!!!",
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.blue,
+                  ),
+                ),
+              ),
       ),
     );
   }
